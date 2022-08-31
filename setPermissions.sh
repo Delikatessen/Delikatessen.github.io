1b5baf175fe72e10e33af6efa4bcac2a5ed278cb
Loren Pontiff <loren.pontiff@student.allenisd.org>
2022-08-31 09:05:46 -0500

    removed errant link from dp

--- setPermissions.sh
+++ setPermissions.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-chmod -R a+rX ~/Digital\ Portfolio
+
+find -L "$PWD" -not -path "*/.*" -type d -exec chmod a+X {} \;
+find -L "$PWD" -not -path "*/.*" -type f \( -name "*.html" -o name "*.png" -o -name "*.jpg" \) -exec chmod a+r {} \;
